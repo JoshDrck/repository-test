e25d810bd076efdb380bf8fc053bbec1622eb8a9
JoshDrck <joseph.64@outlook.com>
2021-07-07 22:20:09 -0500

    Mensaje de tutorial

--- src/epository/test/EpositoryTest.java
+++ src/epository/test/EpositoryTest.java
@@ -16,6 +16,7 @@ public class EpositoryTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Tutorial de GitHub");
     }
     
 }
